0389c44b2252f728c8a75f9d4607c34d25a63aaf
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-02 10:00:23 +0100

    update the App function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+/* eslint-disable */
+import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
 export default App;
