9c653b28164b817b440bd9ac24f38d265867c8f9
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-03 14:57:59 +0100

    refactor the code to use functional component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
-  render() {
+const App = () => {
     return (
       <div className="App">
         <Calculator />
       </div>
     );
   }
-}
+
 export default App;
