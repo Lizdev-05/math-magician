9e3b0f77b003ef183b66df4b6b9a2855b8b59c33
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-09 21:52:54 +0100

    install reat-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
 import Navbar from './components/Navbar';
 
 const App = () => (
   <div className="App">
-    <Navbar />
-    <Calculator />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Calculator />} />
+
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
