eff531d4034e4e148a7b4ae927c8b9aab2b542ec
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-09 21:40:18 +0100

    add styling to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
+import Navbar from './components/Navbar';
 
 const App = () => (
   <div className="App">
+    <Navbar />
     <Calculator />
   </div>
 );
