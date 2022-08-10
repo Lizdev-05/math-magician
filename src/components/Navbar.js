db6357498dc4ebc41d410573b569dc8376fa1b6e
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-10 01:47:48 +0100

    complete homepage and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,46 @@
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div className="navbar">
-    <h2>Math Magician</h2>
-    <ul className="nav-items">
-      <li>
-        <NavLink to="/" className={(navLink) => (navLink.isActive ? 'active' : 'none')}>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/calculate" className={(navLink) => (navLink.isActive ? 'active' : 'none')}>
-          Calculator
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/quote" className={(navLink) => (navLink.isActive ? 'active' : 'none')}>
-          Quotes
-        </NavLink>
-      </li>
-    </ul>
-  </div>
-);
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/calculate',
+      text: 'Calculator',
+    },
+
+    {
+      id: 3,
+      path: '/quotes',
+      text: 'Quotes',
+    },
+  ];
+
+  return (
+    <div className="navbar">
+      <h2>Math Magician</h2>
+      <ul className="nav-items">
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink
+              to={link.path}
+              style={({ isActive }) => ({
+                color: isActive ? 'orangered' : 'white',
+                textDecoration: isActive ? 'underline' : null,
+              })}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
