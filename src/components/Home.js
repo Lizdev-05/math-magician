63f1c37f65d83bd36dea4f5d85da5e3ce2f1ff44
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-10 01:21:13 +0100

    style home page

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home-container">
+    <h1>Welcome to our page</h1>
+    <p>
+      Mathematics is the science that deals with the logic of shape, quantity
+      and arrangement. Mathematics plays a vital role in all aspects of life,
+      whether in everyday matters such as time tracking, driving, cooking, or
+      jobs such as accounting, finance, banking, engineering, and software.
+    </p>
+
+    <p>
+      Mathematics is the cradle of all creations, without which the world cannot
+      move an inch. It makes our life orderly and prevents chaos. Certain
+      qualities that are nurtured by mathematics are power of reasoning,
+      creativity, abstact or spatial thinking, critical thinking and even
+      effective communication.
+    </p>
+  </div>
+);
+
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => (
   <div className="navbar">
-    <Link to="/">Home</Link>
-    <Link to="/calculate">Calculator</Link>
-    <Link to="/quote">Quote</Link>
-
+    <h2>Math Magician</h2>
+    <ul className="nav-items">
+      <li>
+        <NavLink to="/" className={(navLink) => (navLink.isActive ? 'active' : 'none')}>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/calculate" className={(navLink) => (navLink.isActive ? 'active' : 'none')}>
+          Calculator
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/quote" className={(navLink) => (navLink.isActive ? 'active' : 'none')}>
+          Quotes
+        </NavLink>
+      </li>
+    </ul>
   </div>
 );
 
