a4b7c610d7ef19ce842f50a8785ad96028775406
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-10 09:29:20 +0100

    update the api

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -8,11 +8,12 @@ const Quote = () => {
   const colors = ["#ffff00", "#90ee90", "#ffa500", "#ff68ff", "#a9a9e7"];
 
   const getQuotes = () => {
-    fetch('https://type.fit/api/quotes')
+    fetch('https://random-math-quote-api.herokuapp.com/')
     .then((res) => res.json())
   .then((data) => {
-   let randomNum = Math.floor(Math.random() * data.length)
-    setQuotes(data[randomNum]);
+  //  let randomNum = Math.floor(Math.random() * data.length)
+  //   setQuotes(data[randomNum]);
+    setQuotes(data)
   });
   };
 
@@ -26,11 +27,11 @@ const Quote = () => {
   return(
     <>
       <div className='quote'>
-      <p ref={textRef}>{quotes.text}</p>
+      <p ref={textRef}>{quotes.quote}</p>
       <p className='author'>{quotes.author}</p>
     <div>
           <button className='btns quote-btn' onClick={getQuotes}>Get Quotes</button>
-          <a href={`https://twitter.com/intent/tweet?text=${quotes.text}`} target='_blank'
+          <a href={`https://twitter.com/intent/tweet?text=${quotes.quote}`} target='_blank'
             rel='noopener noreferrer'
           className='btns'>Tweet</a>
         </div>
